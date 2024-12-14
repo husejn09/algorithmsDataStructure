a432f0d98ad9b97bd07394ba05309293b6ed64e3
husejn09 <husejn138@outlook.com>
2024-12-14 09:28:00 +0100

    Initial commit

---
+++ src/test/java/com/aqua/algorithmsDataStructures/AlgorithmsDataStructuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aqua.algorithmsDataStructures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsDataStructuresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
